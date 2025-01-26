bee75baaf5a1ffa0684f2a882a09bbf6c50694cd
DESKTOP-A78PJVU\Edward <edwardhsieh031007@gmail.com>
2025-01-25 22:00:00 -0800

    Merge conflict fix

--- Source/bubble/Private/BubbleHUDBase.cpp
+++ Source/bubble/Private/BubbleHUDBase.cpp
@@ -3,3 +3,29 @@
 
 #include "BubbleHUDBase.h"
 
+#include "UI/BaseUILayout.h"
+
+UBaseUILayout* ABubbleHUDBase::ReturnRootLayer()
+{
+	return RootLayer;
+}
+
+void ABubbleHUDBase::BeginPlay()
+{
+	Super::BeginPlay();
+	if(!BaseUILayer)
+	{
+		return;
+	}
+	if (IsValid(GetOwningPlayerController()))
+	{
+		RootLayer = Cast<UBaseUILayout>(CreateWidget(GetOwningPlayerController(), BaseUILayer)) ;
+		if(IsValid(RootLayer))
+		{
+			//DebugUse
+			//GEngine->AddOnScreenDebugMessage(0, 5, FColor::Red, RootLayer->GetName());
+			RootLayer->AddToViewport();
+			
+		}
+	};
+}
